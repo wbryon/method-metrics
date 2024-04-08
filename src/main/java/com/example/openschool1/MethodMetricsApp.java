f3f50df212c09b9b5c76e614d9c305330ee4f7b1
wbryon <tamirbudaev.habibula@gmail.com>
2024-04-08 17:06:39 +0300

    feat: добавлен аннотации в классе MethodMetricsApp

--- src/main/java/com/example/openschool1/MethodMetricsApp.java
+++ src/main/java/com/example/openschool1/MethodMetricsApp.java
@@ -2,12 +2,31 @@ package com.example.openschool1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
+@EnableAspectJAutoProxy
 public class MethodMetricsApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MethodMetricsApp.class, args);
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Async-");
+		executor.initialize();
+		return executor;
 	}
 
 }
