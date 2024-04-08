af878bfa80ce30048953e23bb115ca4d4f67c2d5
wbryon <tamirbudaev.habibula@gmail.com>
2024-04-08 17:05:49 +0300

    feat: добавлен метод findByMethodName в ExecutionTimeRepository

--- src/main/java/com/example/openschool1/repository/ExecutionTimeRepository.java
+++ src/main/java/com/example/openschool1/repository/ExecutionTimeRepository.java
@@ -3,5 +3,8 @@ package com.example.openschool1.repository;
 import com.example.openschool1.model.ExecutionTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExecutionTimeRepository extends JpaRepository<ExecutionTime, Long> {
+    List<ExecutionTime> findByMethodName(String methodName);
 }
