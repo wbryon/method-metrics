92abf3bf2cd078851cf6f0ae7d1e15a3f6c7e6df
wbryon <tamirbudaev.habibula@gmail.com>
2024-04-08 17:04:44 +0300

    feat: реализованы методы в классе ExecutionTimeServiceImpl

--- src/main/java/com/example/openschool1/service/ExecutionTimeServiceImpl.java
+++ src/main/java/com/example/openschool1/service/ExecutionTimeServiceImpl.java
@@ -1,29 +1,38 @@
 package com.example.openschool1.service;
 
+import com.example.openschool1.model.ExecutionTime;
 import com.example.openschool1.repository.ExecutionTimeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.OptionalDouble;
+
 @Service
+@RequiredArgsConstructor
 public class ExecutionTimeServiceImpl implements ExecutionTimeService {
 
     private final ExecutionTimeRepository executionTimeRepository;
 
-    public ExecutionTimeServiceImpl(ExecutionTimeRepository executionTimeRepository) {
-        this.executionTimeRepository = executionTimeRepository;
-    }
-
-    @Override
+    @Async
     public void saveExecutionTime(String methodName, long executionTime, boolean isAsync) {
-
+        ExecutionTime executionTimeRecord = new ExecutionTime(methodName, executionTime, isAsync,
+                ZonedDateTime.now());
+        executionTimeRepository.save(executionTimeRecord);
     }
 
-    @Override
     public double getAverageExecutionTime(String methodName) {
-        return 0;
+        List<ExecutionTime> executionTimes = executionTimeRepository.findByMethodName(methodName);
+        OptionalDouble average = executionTimes
+                .stream()
+                .mapToLong(ExecutionTime::getExecutionTime)
+                .average();
+        return average.isPresent() ? average.getAsDouble() : 0;
     }
 
-    @Override
     public long getTotalCalls(String methodName) {
-        return 0;
+        return executionTimeRepository.findByMethodName(methodName).size();
     }
 }
